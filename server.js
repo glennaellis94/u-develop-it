2e9d405c6a8b6a7240873dff6aadbf731c25c3aa
Glenn Ellis <glennaellis94@gmail.com>
2021-06-13 17:40:05 -0400

    added objects

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+	{
+		host: "localhost",
+		// Your MySQL username,
+		user: "root",
+		// Your MySQL password
+		password: "Webby01!",
+		database: "election",
+	},
+	console.log("Connected to the election database.")
+);
+
 app.get("/", (req, res) => {
 	res.json({
 		message: "Hello World",
 	});
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
 });
 
 // Default response for any other request (Not Found)
